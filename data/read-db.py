6d58766ac378edad3f9a2f2d01416cbb8b18a479
jace-ys <jaceys.tan@gmail.com>
2018-11-01 02:06:03 +0000

    data upload service

---
+++ data/read-db.py
@@ -0,0 +1,17 @@
+import json
+import sqlite3
+
+def main():
+
+    sqlite_conn = sqlite3.connect("readings.sqlite")
+    cursor = sqlite_conn.cursor()
+
+    cursor.execute("SELECT * FROM data")
+    data = cursor.fetchall()
+    print(data)
+
+    sqlite_conn.commit()
+    sqlite_conn.close()
+
+if __name__ == "__main__":
+    main()
