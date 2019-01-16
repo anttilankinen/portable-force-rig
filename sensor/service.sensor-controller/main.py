94174a3a2d248868c9eb5842f44282adf4922a5f
jace-ys <jaceys.tan@gmail.com>
2019-01-16 16:12:41 +0000

    UI button tweaks

--- sensor/service.sensor-controller/main.py
+++ sensor/service.sensor-controller/main.py
@@ -1,29 +1,46 @@
 import os
 import random
+import time
 import redis
 import threading
 from flask import Flask
+from multiprocessing import Process
 
 REDIS_HOST = os.getenv('DOCKER_HOST')
 REDIS_PORT = int(os.getenv('REDIS_PORT'))
 
 app = Flask(__name__)
-redis_client = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=0)
+redis_client = redis.StrictRedis(host='redis', port=REDIS_PORT, db=0)
 reddis_channel = redis_client.pubsub()
 
-# start thread to read from GPIO
+process = None
+
+def read_loop():
+    while True:
+        value = random.uniform(10, 100)
+        redis_client.publish('sensor-data', f'{round(value, 2)}')
+        time.sleep(1)
+
+# start process to read from GPIO
 @app.route('/start')
 def start():
-    # read from GPIO
-    value = random.uniform(10, 100)
-    # publish to redis
-    redis_client.publish('sensor-data', f'{round(value, 2)}')
-    return 'Started reading from sensor..'
+    # # read from GPIO
+    global process
+    if process is None:
+        process = Process(target=read_loop)
+        process.start()
+        return 'Started reading from sensor..'
+    return 'Sensor already running..'
 
-# stop thread to read from GPIO
+# stop process to read from GPIO
 @app.route('/stop')
 def stop():
-    return 'Stopped reading from sensor..'
+    global process
+    if process is not None:
+        process.terminate()
+        process = None
+        return 'Stopped reading from sensor..'
+    return 'Sensor not running..'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
--- sensor/web-client/src/components/App.js
+++ sensor/web-client/src/components/App.js
@@ -6,33 +6,43 @@ export default class App extends Component {
   state = {
     current: [],
     saved: [],
-    showTable: false
+    showTable: false,
+    started: false
   }
 
   startRecording = () => {
+    this.setState({ started: true });
     fetch('/api/sensor-controller/start')
     .then(res => res.text())
     .then(string => console.log(string));
   }
 
   stopRecording = () => {
+    this.setState({ started: false });
     fetch('/api/sensor-controller/stop')
     .then(res => res.text())
     .then(string => console.log(string));
   }
 
   saveData = () => {
-    const JSONdata = JSON.stringify({ data: this.state.current });
+    const { current } = this.state;
+    if (current && current.length) {
+      const JSONdata = JSON.stringify({ data: current });
 
-    fetch('/api/rpi/data', {
-      method: 'post',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSONdata,
-    }).then(res => res.text())
-    .then(string => {
-      console.log(string);
-      this.setState({ current: [] });
-    });
+      fetch('/api/rpi/data', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSONdata,
+      }).then(res => res.text())
+      .then(string => {
+        console.log(string);
+        this.setState({ current: [] });
+      });
+    }
+  }
+
+  clearData = () => {
+    this.setState({ current: [] });
   }
 
   uploadData = () => {
@@ -63,28 +73,36 @@ export default class App extends Component {
   }
 
   render() {
+    const { saved, showTable, started } = this.state;
     return (
       <div style={{ textAlign: 'center', padding: '100px' }}>
         <h1>Portable Force Rig Dashboard</h1>
         <h3>Incoming data: {this.state.current.join(', ')}</h3>
         <div style={{ marginTop: '20px '}}>
-          <button className="ui green button" onClick={this.startRecording}>
-            <i className="play icon"></i>Start
+          {!started &&
+            <button className="ui green button" onClick={this.startRecording}>
+              <i className="play icon"></i>Start
+            </button>
+          }
+          {started &&
+            <button className="ui red button" onClick={this.stopRecording}>
+              <i className="stop icon"></i>Stop
+            </button>
+          }
+          <button className="ui yellow button" onClick={this.clearData} disabled={started}>
+            <i className="trash icon"></i>Clear
           </button>
-          <button className="ui red button" onClick={this.stopRecording}>
-            <i className="stop icon"></i>Stop
-          </button>
-          <button className="ui yellow button" onClick={this.saveData}>
+          <button className="ui blue button" onClick={this.saveData} disabled={started}>
             <i className="download icon"></i>Save
           </button>
-          <button className="ui blue button" onClick={this.uploadData}>
+          <button className="ui brown button" onClick={this.uploadData}>
             <i className="upload icon"></i>Upload
           </button>
           <button className="ui black button" onClick={this.showHistory}>
             <i className="list alternate outline icon"></i>History
           </button>
         </div>
-        {this.state.showTable && <Table tableData={this.state.saved}/>}
+        {showTable && <Table tableData={saved}/>}
       </div>
     );
   }
