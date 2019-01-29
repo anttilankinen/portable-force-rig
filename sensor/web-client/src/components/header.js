492110d33ea8eb606a8184b09c420de4d5d75c97
Rachel Tan <rtsscy@gmail.com>
2019-01-29 19:27:11 +0000

    header cleanup

--- sensor/web-client/src/components/header.js
+++ sensor/web-client/src/components/header.js
@@ -3,8 +3,15 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div  className = "ui secondary pointing menu">
+    <div className = "ui secondary pointing menu" style={{ textAlign: 'center', padding: '50px' }}>
       <Link to ="/"><h1>Portable Force Rig Dashboard</h1></Link>
+      <div className = "right menu">
+      <Link to="/history" className="ui black button"
+      // onClick={this.showHistory}
+      >
+        <i className="list alternate outline icon"></i>History
+      </Link>
+      </div>
     </div>
 
   )
