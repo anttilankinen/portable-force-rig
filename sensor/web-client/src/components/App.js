f78ef889de7d4859de96c315defe96117f01ba6e
jace-ys <jaceys.tan@gmail.com>
2019-01-30 16:43:53 +0000

    streaming

--- sensor/web-client/src/components/App.js
+++ sensor/web-client/src/components/App.js
@@ -1,116 +1,24 @@
 import React, { Component } from 'react';
-import { Line } from 'react-chartjs-2';
-import socketIOClient from 'socket.io-client';
-import Table from './Table';
-import { chartOptions, chartData } from './chartSettings';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-export default class App extends Component {
-  state = {
-    current: [],
-    saved: [],
-    showTable: false,
-    started: false
-  }
+import Dashboard from './pages/Dashboard';
+import History from './pages/History';
+import Header from './Header';
 
-  startRecording = () => {
-    this.setState({ started: true });
-    fetch('/api/sensor-controller/start')
-    .then(res => res.text())
-    .then(string => console.log(string));
-  }
-
-  stopRecording = () => {
-    this.setState({ started: false });
-    fetch('/api/sensor-controller/stop')
-    .then(res => res.text())
-    .then(string => console.log(string));
-  }
-
-  saveData = () => {
-    const { current } = this.state;
-    if (current && current.length) {
-      const JSONdata = JSON.stringify({ data: current });
-
-      fetch('/api/rpi/data', {
-        method: 'post',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSONdata,
-      }).then(res => res.text())
-      .then(string => {
-        console.log(string);
-        this.setState({ current: [] });
-      });
-    }
-  }
-
-  clearData = () => {
-    this.setState({ current: [] });
-  }
-
-  uploadData = () => {
-    fetch('/api/data-upload/')
-    .then(res => res.text())
-    .then(string => console.log(string));
-  }
-
-  showHistory = () => {
-    fetch('/api/rpi/data')
-    .then(res => res.json())
-    .then(json => {
-      const history = json.rows.map(row => ({
-        date: row.date_time,
-        antSize: row.ant_size,
-        readings: row.readings
-      }))
-      this.setState({ saved: history, showTable: !this.state.showTable });
-    });
-  }
-
-  componentDidMount() {
-    const socket = socketIOClient('http://localhost:7002');
-    socket.on('connected', message => console.log(message));
-    socket.on('new data', message => {
-      this.setState({ current: [...this.state.current, message.data] });
-    });
-  }
-
-  render() {
-    const { current, saved, showTable, started } = this.state;
-
-    let dataToDisplay = current.slice(-20);
-    chartData.labels = dataToDisplay.map((value, index) => (index * 0.025).toFixed(2));
-    chartData.datasets[0].data = dataToDisplay;
-
+class App extends Component {
+  render(){
     return (
-      <div style={{ textAlign: 'center', padding: '100px' }}>
-        <h1>Portable Force Rig Dashboard</h1>
-        <div><Line data={chartData} options={chartOptions} height={350}/></div>
-        <div style={{ marginTop: '20px '}}>
-          {!started &&
-            <button className="ui green button" onClick={this.startRecording}>
-              <i className="play icon"></i>Start
-            </button>
-          }
-          {started &&
-            <button className="ui red button" onClick={this.stopRecording}>
-              <i className="stop icon"></i>Stop
-            </button>
-          }
-          <button className="ui yellow button" onClick={this.clearData} disabled={started}>
-            <i className="trash icon"></i>Clear
-          </button>
-          <button className="ui blue button" onClick={this.saveData} disabled={started}>
-            <i className="download icon"></i>Save
-          </button>
-          <button className="ui brown button" onClick={this.uploadData}>
-            <i className="upload icon"></i>Upload
-          </button>
-          <button className="ui black button" onClick={this.showHistory}>
-            <i className="list alternate outline icon"></i>History
-          </button>
-        </div>
-        {showTable && <Table tableData={saved}/>}
+      <div className="ui container">
+        <BrowserRouter>
+          <div>
+            <Header/>
+            <Route path="/" exact component={Dashboard} />
+            <Route path="/history" exact component={History} />
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
 }
+
+export default App;
---
+++ sensor/web-client/src/components/pages/Dashboard.js
@@ -0,0 +1,95 @@
+import React, { Component } from 'react';
+import { Line } from 'react-chartjs-2';
+import socketIOClient from 'socket.io-client';
+import { chartOptions, chartData } from '../chartSettings';
+
+export default class Dashboard extends Component {
+  state = {
+    current: [],
+    started: false
+  }
+
+  startRecording = () => {
+    this.setState({ started: true });
+    fetch('/api/sensor-controller/start')
+    .then(res => res.text())
+    .then(string => console.log(string));
+  }
+
+  stopRecording = () => {
+    this.setState({ started: false });
+    fetch('/api/sensor-controller/stop')
+    .then(res => res.text())
+    .then(string => console.log(string));
+  }
+
+  saveData = () => {
+    const { current } = this.state;
+    if (current && current.length) {
+      const JSONdata = JSON.stringify({ data: current });
+
+      fetch('/api/rpi/data', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSONdata,
+      }).then(res => res.text())
+      .then(string => {
+        console.log(string);
+        this.setState({ current: [] });
+      });
+    }
+  }
+
+  clearData = () => {
+    this.setState({ current: [] });
+  }
+
+  uploadData = () => {
+    fetch('/api/data-upload/')
+    .then(res => res.text())
+    .then(string => console.log(string));
+  }
+
+  componentDidMount() {
+    const socket = socketIOClient('http://localhost:7002');
+    socket.on('connected', message => console.log(message));
+    socket.on('new data', message => {
+      this.setState({ current: [...this.state.current, message.data] });
+    });
+  }
+
+  render() {
+    const { current, started } = this.state;
+    chartData.labels = current.map((value, index) => (index * 0.025).toFixed(3));
+    chartData.datasets[0].data = current;
+
+    return (
+      <div style={{ textAlign: 'center', padding: '20px' }}>
+        <h4>Status</h4>
+        <div><Line data={chartData} options={chartOptions} height={350}/></div>
+        <div style={{ margin: '20px 0 40px 0'}}>
+          {!started &&
+            <button className="ui green button" onClick={this.startRecording}>
+              <i className="play icon"></i>Start
+            </button>
+          }
+          {started &&
+            <button className="ui red button" onClick={this.stopRecording}>
+              <i className="stop icon"></i>Stop
+            </button>
+          }
+          <button className="ui yellow button" onClick={this.clearData} disabled={started}>
+            <i className="trash icon"></i>Clear
+          </button>
+          <button className="ui blue button" onClick={this.saveData} disabled={started}>
+            <i className="download icon"></i>Save
+          </button>
+          <button className="ui brown button" onClick={this.uploadData}>
+            <i className="upload icon"></i>Upload
+          </button>
+        </div>
+        <img src="http://localhost:8000/stream.mjpg" alt="Video stream" width="640" height="480" />
+      </div>
+    );
+  }
+}
