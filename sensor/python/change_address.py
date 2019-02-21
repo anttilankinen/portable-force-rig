b8efcc784741ff522d6d3b41e06ae9718202aece
anttilankinen <antti.lankinen15@imperial.ac.uk>
2019-02-21 10:29:54 +0000

    add safety check for address change

--- sensor/python/change_address.py
+++ sensor/python/change_address.py
@@ -18,11 +18,14 @@ if __name__ == '__main__':
     bus = int(args.bus)
     old_address = int(args.old, 0)
     new_address = int(args.new, 0)
-    print(new_address, old_address)
-    try:
-        DEV_CTX = smbus2.SMBus(bus)
-        DEV_CTX.write_i2c_block_data(old_address, 0x00, [0x02, 0x00, 0x01,
-            new_address, 0xff])
-    except IOError as e:
-        print(e.message)
-        sys.exit(1)
+    if input('Bus: %i Old address: %i New address: %i. Make change? (y/n)'
+            % (bus, old_address, new_address)) == 'y':
+        try:
+            DEV_CTX = smbus2.SMBus(bus)
+            DEV_CTX.write_i2c_block_data(old_address, 0x00, [0x02, 0x00, 0x01,
+                new_address, 0xff])
+        except IOError as e:
+            print(e.message)
+            sys.exit(1)
+    else:
+        print('Aborting address change.')
